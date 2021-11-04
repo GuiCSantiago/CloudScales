a0cbcf518a70b2a44dba4162f7e56de3688a302b
Guilherme Santiago <santiagoguilhermecosta@gmail.com>
2021-11-03 21:31:28 -0300

    Pastas DAO e ViewModels padrões

---
+++ CloudScales/DAO/ConexaoBD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCJogos.DAO
+{
+    public static class ConexaoBD
+    {
+        public static SqlConnection GetConexao()
+        {
+            string strCon = "Data Source=DESKTOP-GUILHER\\SQLEXPRESS; Database=CloudScalesDB; user id=sa; password=123456";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
